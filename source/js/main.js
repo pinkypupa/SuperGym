bf3187de7fdaff6c3801a10acf1002f95a6d6865
Leisan <lgabdrakhmanova@gmail.com>
2023-11-02 19:40:23 +0500

    Добавляет блок Абонементы для разрешения Десктоп (Компьютер)

--- source/js/main.js
+++ source/js/main.js
@@ -1,6 +1,11 @@
-import {iosVhFix} from './utils/ios-vh-fix';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
+import { iosVhFix } from './utils/ios-vh-fix';
+import { initModals } from './modules/modals/init-modals';
+import { Form } from './modules/form-validate/form';
+
+// пока сделаем через ид, потом надо через дата-атрибуты как-то
+const videoWrapper = document.querySelector('.gym__video');
+const videoPlayer = document.getElementById('videoPoster');
+const playButton = document.getElementById('playButton');
 
 // ---------------------------------
 
@@ -14,14 +19,29 @@ window.addEventListener('DOMContentLoaded', () => {
   // Modules
   // ---------------------------------
 
+  videoWrapper.addEventListener('click', () => {
+    videoPlayer.remove();
+    playButton.remove();
+    const iframePlayer = document.createElement('iframe');
+    iframePlayer.setAttribute('src', 'https://www.youtube.com/embed/9TZXsZItgdw?si=twR-ewfRYAOdjvuk?&autoplay=1');
+    iframePlayer.setAttribute('title', 'YouTube video player');
+    iframePlayer.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share');
+    iframePlayer.setAttribute('frameborder', '0');
+    iframePlayer.setAttribute('allowfullscreen', '');
+    iframePlayer.setAttribute('width', '364');
+    iframePlayer.setAttribute('height', '228');
+    videoWrapper.append(iframePlayer);
+  });
+
+
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
-  window.addEventListener('load', () => {
-    initModals();
-    const form = new Form();
-    window.form = form;
-    form.init();
-  });
+  // window.addEventListener('load', () => {
+  //   initModals();
+  //   const form = new Form();
+  //   window.form = form;
+  //   form.init();
+  // });
 });
 
 // ---------------------------------
